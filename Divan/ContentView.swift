a3afa415a4a5562a4b292a3c989f7a13c7ebd57b
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-03-11 14:33:23 +0330

    Initial Commit

---
+++ Divan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Divan
+//
+//  Created by Sothesom on 21/12/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Divan/DivanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivanApp.swift
+//  Divan
+//
+//  Created by Sothesom on 21/12/1403.
+//
+
+import SwiftUI
+
+@main
+struct DivanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
